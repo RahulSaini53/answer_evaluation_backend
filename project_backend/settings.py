b8e0308e1be9a39e0ce49e38d3bb909d15e98d30
RahulSaini53 <rksckn123@gmail.com>
2024-06-06 10:53:59 +0530

    files updated

--- project_backend/settings.py
+++ project_backend/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/Media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
